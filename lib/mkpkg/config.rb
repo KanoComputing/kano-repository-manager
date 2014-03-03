1e7db5413d788c201be14cbf37b1178f8eb2e2f4
Radek Pazdera <radek@kano.me>
2014-03-03 17:05:41 +0000

    drepo: Adding a configuration file support
    You can now set (system-wide or locally) default parameters for drepo
    through a configuration file. They're being loaded from:
    
        /etc/drepo.conf
        ~/.drepo.conf

---
+++ lib/mkpkg/config.rb
@@ -0,0 +1,44 @@
+require "yaml"
+
+module Mkpkg
+  class Config
+    attr_reader :default_repo, :repositories
+
+    def initialize(locations)
+      @default_repo = nil
+      @repositories = {}
+
+      locations.each do |conf_file|
+        conf_file = File.expand_path conf_file
+        next unless File.exists? conf_file
+        load conf_file
+      end
+    end
+
+    private
+    def load(path)
+      conf_file = YAML::load_file path
+
+      if conf_file.has_key? "repositories"
+        if conf_file["repositories"].is_a? Array
+          conf_file["repositories"].each do |repo|
+            raise "Repo name missing in the config." unless repo.has_key? "name"
+            raise "Repo location missing in the config" unless repo.has_key? "location"
+            @repositories[repo["name"]] = {
+              :location => repo["location"]
+            }
+          end
+        else
+          raise "The 'repositories' config option must be an array."
+        end
+      end
+
+      if conf_file.has_key? "default_repo"
+        @default_repo = conf_file["default_repo"]
+        unless @repositories.has_key? @default_repo
+          raise "Default repo #{@default_repo} doesn't exist"
+        end
+      end
+    end
+  end
+end
