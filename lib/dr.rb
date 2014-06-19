164d3d5cbfbc29f8731043c9057d0df40554fbcf
Radek Pazdera <radek@kano.me>
2014-06-19 17:12:36 +0100

    Adding check for dependencies
    To make sure all the required programs are installed. A warning is
    printed in case they're not.
    
    Fixes #21

--- lib/dr.rb
+++ lib/dr.rb
@@ -3,7 +3,19 @@
 
 require "dr/version"
 require "dr/repo"
+require "dr/logger"
 
 module Dr
-  # Your code goes here...
+  def self.check_dependencies(deps=[])
+    ENV["PATH"].split(File::PATH_SEPARATOR).each do |path_dir|
+      deps.delete_if do |dep_name|
+        Dir[File.join(path_dir, dep_name)].length > 0
+      end
+    end
+
+    if deps.length > 0
+      Logger.log :warn, "Missing some dependencies:"
+      deps.each { |dep| Logger.log :warn, "  #{dep.fg "red"}" }
+    end
+  end
 end
