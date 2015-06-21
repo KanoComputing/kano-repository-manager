c8ff25e93a43fe6fe4ce0d632984b4a84e64d74d
Radek Pazdera <radek@kano.me>
2015-06-21 14:01:13 +0100

    Finalising the serve command

--- lib/dr/server.rb
+++ lib/dr/server.rb
@@ -5,20 +5,18 @@ require "rack"
 
 module Dr
   class Server
-    def initialize(port, root_route, archive_path)
+    def initialize(port, root_route, address, archive_path)
       @port = port
-      @route = root_route
-      @archive = archive_path
+      @host = address
+      @dir_server = Rack::Builder.new do
+        map root_route do
+          run Rack::Directory.new(archive_path)
+        end
+      end
     end
 
     def start
-      dir_server = Rack::Builder.new do
-        map @route do
-          run Rack::Directory.new @archive_path
-        end
-      end
-
-      Rack::Handler::WEBrick.run dir_server, :port => @port
+      Rack::Handler::Thin.run(@dir_server, :Port => @port, :Host => @host)
     end
   end
 end
