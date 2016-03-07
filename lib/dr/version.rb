8963bbd96404ae0c9714f7bc6da9aeb9e808aaf5
Radek Pazdera <radek@kano.me>
2016-03-07 12:20:44 +0000

    Bumping to 1.3.2

--- lib/dr/version.rb
+++ lib/dr/version.rb
@@ -1,6 +1,6 @@
-# Copyright (C) 2014 Kano Computing Ltd.
-# License: http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+# Copyright (C) 2014-2016 Kano Computing Ltd.
+# License: http://www.gnu.org/licenses/gpl-2.0.txt GNU GPL v2
 
 module Dr
-  VERSION = "1.3.1"
+  VERSION = "1.3.2"
 end
