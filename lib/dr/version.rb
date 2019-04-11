61d41b674e753710e7afe2d26bc25effe6a20602
Tom Bettany <tom@kano.me>
2019-04-11 10:53:22 +0100

    Sources: Build from Kano mirrors instead of 3rd party ones
    Kano has a full mirror of the Raspbian sources which can guarantee
    reproducible builds but the packages were using the 3rd party sources.
    To change the build root to use the Kano sources, add an option to the
    build sources to mark them as being for `build_only` and also completely
    rewrite the `debootstrap`ed sources to ensure that the final sources
    contain exactly what is expected.

--- lib/dr/version.rb
+++ lib/dr/version.rb
@@ -1,6 +1,6 @@
-# Copyright (C) 2014-2018 Kano Computing Ltd.
+# Copyright (C) 2014-2019 Kano Computing Ltd.
 # License: http://www.gnu.org/licenses/gpl-2.0.txt GNU GPL v2
 
 module Dr
-  VERSION = "1.3.4"
+  VERSION = "1.3.5"
 end
